1e4c317df2ee37583e978aafde06028b2970b17e
godleydemon <giantmallet@gmail.com>
2018-06-15 23:36:56 -0800

    fixed java install and build tools install

--- gaia.sh
+++ gaia.sh
@@ -51,7 +51,15 @@ while true;do
       fi
 
       if java -version | grep -q "java version" ; then
-
+        echo "Java NOT installed! WTF!"
+        echo "Installing Java8"
+        apt-get install -y python-software-properties debconf-utils > /dev/null
+        add-apt-repository ppa:webupd8team/java > /dev/null
+        apt-get update > /dev/null
+        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
+        apt-get install -y oracle-java8-installer > /dev/null
+        echo "Finished installing Java8"
       else
         echo "Java NOT installed! WTF!"
         exit 0
@@ -115,25 +123,28 @@ while true;do
       exit 0 ;;
     [2]*)
       apt-get update
+      echo "Installing a few packages, please wait"
+      apt-get install -y software-properties-common joe git expect htop unzip make python-software-properties python-dev python-twisted-core python-twisted-web python-twisted-words libssl-dev python-pip software-properties-common gem ufw > /dev/null
+      pip install urwid feedparser psutil
+      gem install haste system_timer bundler > /dev/null
+      git config --global core.editor "nano"
+      echo "done installing packages"
       if java -version | grep -q "java version" ; then
         echo "Java was found, not installing"
       else
         echo "Java NOT installed! WTF!"
         echo "Installing Java8"
         apt-get install -y python-software-properties debconf-utils > /dev/null
-        add-apt-repository ppa:webupd8team/java > /dev/null
+        echo deb http://ftp.debian.org/debian jessie-backports main >> /etc/apt/sources.list
         apt-get update > /dev/null
-        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
-        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
-        apt-get install -y oracle-java8-installer > /dev/null
+		echo "line 140"
+        echo "openjdk-8-jdk-headless  shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+        echo "openjdk-8-jdk-headless  shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
+		apt-get install -t jessie-backports ca-certificates-java
+        apt-get install -y openjdk-8-jdk-headless 
         echo "Finished installing Java8"
       fi
-      echo "Installing a few packages, please wait"
-      apt-get install -y joe git expect htop unzip make python-software-properties python-dev python-twisted-core python-twisted-web python-twisted-words libssl-dev python-pip software-properties-common gist-paste gem ufw > /dev/null
-      pip install urwid feedparser psutil
-      gem install haste system_timer bundler > /dev/null
-      git config --global core.editor "nano"
-      echo "done installing packages"
+
       IP=`hostname -i`
       sed -i "s/#ListenAddress 0.0.0.0/ListenAddress $IP/g" /etc/ssh/sshd_config
       sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
@@ -147,18 +158,19 @@ while true;do
       exit 0 ;;
     [3]*)
       echo "*--==Installing BuildTools==--*"
-      apt-get update
       if java -version | grep -q "java version" ; then
         echo "Java was found, not installing"
       else
         echo "Java NOT installed! WTF!"
         echo "Installing Java8"
         apt-get install -y python-software-properties debconf-utils > /dev/null
-        add-apt-repository ppa:webupd8team/java > /dev/null
+        echo deb http://ftp.debian.org/debian jessie-backports main >> /etc/apt/sources.list
         apt-get update > /dev/null
-        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
-        echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
-        apt-get install -y oracle-java8-installer > /dev/null
+		echo "line 140"
+        echo "openjdk-8-jdk-headless  shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
+        echo "openjdk-8-jdk-headless  shared/accepted-oracle-license-v1-1 seen true" | debconf-set-selections
+		apt-get install -t jessie-backports ca-certificates-java
+        apt-get install -y openjdk-8-jdk-headless 
         echo "Finished installing Java8"
       fi
 	  mkdir /home/buildtools
